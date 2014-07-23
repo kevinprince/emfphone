640d45f2cac5c407e072c99df635bb42b95cf555
kevinprince <nextgengames@gmail.com>
2014-07-23 20:23:09 +0100

    missing semicolons

--- scripts/subscriber.js
+++ scripts/subscriber.js
@@ -6,16 +6,16 @@ subscriber.prototype = new Object;
 
 subscriber.prototype.create = function(id) {
   return this.db.query_insert('INSERT INTO users (username) VALUES($)', [id]);
-}
+};
 
 subscriber.prototype.get = function(id) {
   return this.db.get_one_line_assoc('SELECT * FROM users WHERE id=$', [id]);
-}
+};
 
 subscriber.prototype.user_exists = function(username) {
   return this.db.get_one_line_assoc('SELECT * FROM users WHERE username=$', [username]);
-}
+};
 
 subscriber.prototype.update_location = function(username, location) {
   return this.db.query('UPDATE user SET location=$ WHERE username=$', [location, username]);
-}
+};
