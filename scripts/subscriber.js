1f07db007f6be730405ea2b513f66262e847f25f
Kevin Prince <nextgengames@gmail.com>
2014-07-23 16:56:32 +0100

    Update subscriber.js

---
+++ scripts/subscriber.js
@@ -0,0 +1,21 @@
+function subscriber(){
+  db = new db_client('emf', 'mysql');
+}
+
+subscriber.prototype = new Object;
+
+subscriber.prototype.create = function(id) {
+  return this.db.query_insert('INSERT INTO users (username) VALUES($)', [id]);
+}
+
+subscriber.prototype.get = function(id) {
+  return this.db.get_one_line_assoc('SELECT * FROM users WHERE id=$', [id]);
+}
+
+subscriber.prototype.user_exists = function(username) {
+  return this.db.get_one_line_assoc('SELECT * FROM users WHERE username=$', [username]);
+}
+
+subscriber.prototype.update_location = function(username, location) {
+  return this.db.query('UPDATE user SET location=$ WHERE username=$', [location, username]);
+}
